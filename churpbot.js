bb2e4d55ca46944163f8701fbb7c50543dcf0964
Greyson Von Hagel <gvonhagel@gmail.com>
2016-05-12 16:22:49 -0500

    + Additional comments to explain logic progression

--- churpbot.js
+++ churpbot.js
@@ -44,32 +44,40 @@ stream.on('tweet', function (tweet) {
     if (args.length < 3) {
         response = "Error! Invalid number of arguments. IGNORE";
     }
+    //Is the second argument a username?
     else if (!args[1].startsWith("@")) {
         response = "Error! Invalid recipient formatting. IGNORE";
     }
+    //Is the third argument a number?
     else if (isNaN(+args[2].replace("$", ""))) {
         response = "Are you trying to use Churp? If so, please make sure your dollar amount is in the right format (IE. $15/$15.00)";
     }
+    //Does the third argument have the proper money formatting?
     else if (!args[2].startsWith("$")) {
         response = "Are you trying to use Churp? If so, please make sure your dollar amount is in the right format (IE. $15/$15.00)";
     } else {
+        //Does the user sending the Churp have an account on churp.me?
         userExists(tweet.user.screen_name).then(function (success) {
             if (!success) {
                 response = "Are you trying to use Churp? If so, you must first create an account on our website http://churp.me";
             }
             else {
+                //Does the user receiving the Churp have an account on churp.me?
                 userExists(args[1].replace("@", "")).then(function (success) {
                     if (!success) {
                         response = "Sorry, we could not find a Churp account linked to that user name. If you think this is an error, please let us know";
                     } else {
                         getBalance(tweet.user.screen_name).then(function (fBal) {
+                            //Does the user sending the Churp have a linked payment account with a sufficient balance?
                             if (fBal <= 0 || +args[2].replace("$", "") > fBal) {
                                 response = "Sorry, but you do not appear to have sufficient funds to send this Churp";
                             } else {
                                 getBalance(args[1].replace("@", "")).then(function (tBal) {
+                                    //Does the user receiving the Churp have a linked payment account?
                                     if (tBal <= 0) {
                                         response = "Sorry, but it appears that recipient has not yet linked a payment account to their Churp account";
                                     } else {
+                                        //If all of the above are false, send the Churp and transfer the money
                                         doTransfer(tweet.user.screen_name, args[1].replace("@", ""), fBal, tBal, +args[2].replace("$", ""));
                                         response = "Success! Your Churp for " + args[2] + " has been sent to " + args[1];
                                     }
@@ -85,11 +93,14 @@ stream.on('tweet', function (tweet) {
     //After 5 seconds check the response and reply if the response is not to be ignored
     setTimeout(function () {
         console.log("RESPONSE: " + response);
+        //Does the response have an IGNORE tag?
         if (response != null && !response.endsWith("IGNORE")) {
+            //If the response is not ignored, post the response in reply to the initial tweet
             T.post('statuses/update', {
                 status: "@" + tweet.user.screen_name + " " + response,
                 in_reply_to_status_id: tweet.id_str
             }, function (err, data, response) {
+                console.log(err);
             })
         }
     }, 5 * 1000);
